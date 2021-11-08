fd1e6747834ab9dabab23aeea2a6bfacd923725a
Ki Woong Lee <59480963+lightixxx@users.noreply.github.com>
2021-11-08 20:55:29 +0900

    🚧 React Router 3 : URL 파라미터로 상세페이지 100개 만들기 학습

--- src/App.js
+++ src/App.js
@@ -63,8 +63,8 @@ function App() {
           </div>
         </Route>
 
-        <Route path="/detail">
-          <Detail />
+        <Route path="/detail/:id">
+          <Detail shoes={shoes} />
         </Route>
 
         <Route path="/:id">
--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,7 +1,13 @@
-import { useHistory } from 'react-router-dom'
+// import { useState } from 'react'
+import { useHistory, useParams } from 'react-router-dom'
 
-const Detail = () => {
+const Detail = ({ shoes }) => {
+  let { id } = useParams()
   let history = useHistory()
+
+  let 찾은상품 = shoes.find((상품) => {
+    return 상품.id == id
+  })
 
   return (
     <div className="container">
@@ -13,16 +19,16 @@ const Detail = () => {
           />
         </div>
         <div className="col-md-6 mt-4">
-          <h4 className="pt-5">상품명</h4>
-          <p>상품설명</p>
-          <p>120000원</p>
+          <h4 className="pt-5">{찾은상품.title}</h4>
+          <p>{찾은상품.content}</p>
+          <p>{찾은상품.price}</p>
           <button className="btn btn-primary">주문하기</button>
           <button
             className="btn btn-danger"
             onClick={() => {
               history.goBack()
               // 특정 경로로 이동시키려면
-              // history.push('/wow')
+              // history.push('/hello')
             }}
           >
             뒤로가기
