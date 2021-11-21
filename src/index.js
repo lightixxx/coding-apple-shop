a99a1c0ae30da96a7966027f3ebb35e618f2683d
Ki Woong Lee <59480963+lightixxx@users.noreply.github.com>
2021-11-21 20:44:33 +0900

    🚧 세계최고로 쉬운 Redux 2 : reducer/dispatch로 데이터 수정하는 법 학습

--- src/index.js
+++ src/index.js
@@ -8,9 +8,21 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 
-let store = createStore(() => {
-  return [{ id: 0, name: '멋진신발', quan: 2 }]
-})
+const 기본state = [{ id: 0, name: '멋진신발', quan: 2 }]
+const reducer = (state = 기본state, 액션) => {
+  if (액션.type === '수량증가') {
+    let copy = [...state]
+    copy[0].quan++
+    return copy
+  } else if (액션.type === '수량감소') {
+    let copy = [...state]
+    copy[0].quan--
+    return copy
+  } else {
+    return state
+  }
+}
+let store = createStore(reducer)
 
 ReactDOM.render(
   <React.StrictMode>
